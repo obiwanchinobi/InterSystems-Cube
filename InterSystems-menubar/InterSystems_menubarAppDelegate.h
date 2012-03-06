cae1e2a9aa8db460f5a6ca2c38003df86ac7016a
Dave Tong <david.mh.tong@gmail.com>
2012-03-06 17:31:52 +1100

    Initialise project

---
+++ InterSystems-menubar/InterSystems_menubarAppDelegate.h
@@ -0,0 +1,19 @@
+#import <Cocoa/Cocoa.h>
+
+@interface InterSystems_menubarAppDelegate : NSObject <NSApplicationDelegate> {
+@private
+    NSWindow *window;
+    NSPersistentStoreCoordinator *__persistentStoreCoordinator;
+    NSManagedObjectModel *__managedObjectModel;
+    NSManagedObjectContext *__managedObjectContext;
+}
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:sender;
+
+@end
---
+++ InterSystems-menubar/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  InterSystems-menubar
+//
+//  Created by Dave Tong on 6/03/12.
+//  Copyright 2012 David Tong. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
